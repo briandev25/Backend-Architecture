b6f331f650c3d4312fb6ea647149aaa559306a76
briandev25 <briankips34@gmail.com>
2026-04-14 23:05:38 +0300

    completed user controller

---
+++ src/controllers/userController.ts
@@ -0,0 +1,28 @@
+import type {Request,Response} from 'express'
+import User from '../models/userModel.js';
+import validateNewUser from '../schemas/userSchema.js';
+
+
+export const newUserController = async(req:Request,res:Response) =>{
+
+     const result = validateNewUser.safeParse(req.body);
+     if(!result.success){
+          return res.status(400).json({error:result.error.flatten()})
+     }
+      const { username, email, password} =result.data   //Sanitized
+     
+     try{
+       //Check if user exists
+       let userExists = await User.findOne({ email});
+       if(userExists){
+          return res.status(400).json({message:"User already exists, try signing in"});
+       }
+       const newUser = new User({username,email,password});
+       //Saving to DB
+       await newUser.save();
+       res.status(201).json({message:"User saved successfully"})
+
+     }catch(err:any){
+          res.status(500).json({message:err.message})
+     }
+}
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,33 +1,11 @@
 import express from 'express';
-import type {Request,Response} from 'express'
-import User from '../models/userModel.js';
-import validateNewUser from '../schemas/userSchema.js';
+import { newUserController } from '../controllers/userController.js';
+
+
 
 const route = express.Router()
 
-route.post('/register', async(req:Request,res:Response) =>{
-
-     const result = validateNewUser.safeParse(req.body);
-     if(!result.success){
-          return res.status(400).json({error:result.error.flatten()})
-     }
-      const { username, email, password} =result.data   //Sanitized
-     
-     try{
-       //Check if user exists
-       let userExists = await User.findOne({ email});
-       if(userExists){
-          return res.status(400).json({message:"User already exists, try signing in"});
-       }
-       const newUser = new User({username,email,password});
-       //Saving to DB
-       await newUser.save();
-       res.status(201).json({message:"User saved successfully"})
-
-     }catch(err:any){
-          res.status(500).json({message:err.message})
-     }
-})
+route.post('/register',newUserController )
 
 
 
