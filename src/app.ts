a586ad775d748cc3c0217f613878158358323b0d
briandev25 <briankips34@gmail.com>
2026-04-15 18:28:09 +0300

    Added documentation with swagger

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose'
 import dotenv from 'dotenv';
+import swaggerJsDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
 
 import routes from './routes/index.js';
 
@@ -9,8 +11,23 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+const swaggerOptions ={
+    definition:{
+        info:{
+            title:"Backend Archetecture API",
+            version:"1.0.0",
+            description:"A simple Express API for backend archetecture project"
+        }
+    },
+    apis:["./src/routes/*.ts"]
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+
 app.use(express.json());
 app.use('/api/v1',routes);
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs));
 
 
 app.get("/", (req,res) =>{
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,10 +1,42 @@
 import express from 'express';
 import { newUserController } from '../controllers/userController.js';
 
-
-
 const route = express.Router()
 
+/**
+ * @swagger
+ * /api/v1/user/register:
+ *   post:
+ *     summary: Create a new user
+ *     description: Adds a new user to the database
+ *     tags:
+ *       - Users
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - email
+ *               - password
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: john_doe
+ *               email:
+ *                 type: string
+ *                 example: johndoe@example.com
+ *               password:
+ *                 type: string
+ *                 example: strongpassword123
+ *     responses:
+ *       201:
+ *         description: User created successfully
+ *       400:
+ *         description: User already exist or bad request
+ */
 route.post('/register',newUserController )
 
 
